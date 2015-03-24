863e034c60d722de65a0a7591e40abe9feb31adf
unknown <tuanaphan@SGH504PT7D.kms.com.vn>
2015-03-24 17:45:27 +0700

    correct indentation

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -3,30 +3,32 @@
 var savControllers = angular.module('savControllers', []);
 
 // HomeController
-savControllers.controller('HomeCtrl', ['$scope','CodeGeneration','$state',function($scope,CodeGeneration,$state) {
-	// Create Secret Code
-	// This code will be used to access vote or generate Vote URL
-	$scope.secretCode = CodeGeneration.generate();
-	$scope.$state = $state;
-    $scope.taoVote = function() {
-    	$state.transitionTo('step1');
-    };
-}]);
+savControllers.controller('HomeCtrl', ['$scope', 'CodeGeneration', '$state',
+	function($scope,CodeGeneration,$state) {
+		// Create Secret Code
+		// This code will be used to access vote or generate Vote URL
+		$scope.secretCode = CodeGeneration.generate();
+		$scope.$state = $state;
+	    $scope.taoVote = function() {
+	    	$scope.$state.transitionTo('step1');
+	    };
+	}]);
 
 // CreateVoteController
-savControllers.controller('CreateVoteCtrl', ['$scope','ActionURL','$http', function($scope,ActionURL,$http,CodeGeneration) {
+savControllers.controller('CreateVoteCtrl', ['$scope', 'ActionURL', '$http', 
+	function($scope,ActionURL,$http,CodeGeneration) {
     
-	var returnMsg = "";
-	// Post the request to back-end API
-	$http.post(ActionURL, { secret : secretCode }).
-	  success(function(data) {
-		  returnMsg = "Vót được tạo thành công.";
-	  }).
-	  error(function(data, status, headers, config) {
-		  returnMsg = "Đã có lỗi khi tạo Vót.";
-	  });
+		var returnMsg = "";
+		// Post the request to back-end API
+		$http.post(ActionURL, { secret : secretCode }).
+		  success(function(data) {
+			  returnMsg = "Vót được tạo thành công.";
+		  }).
+		  error(function(data, status, headers, config) {
+			  returnMsg = "Đã có lỗi khi tạo Vót.";
+		  });
     
-}]);
+	}]);
 
 savControllers.controller('ConfigVoteCtrl', ['$scope', 
 	function($scope) {
--- src/main/webapp/js/routes.js
+++ src/main/webapp/js/routes.js
@@ -1,30 +1,29 @@
 savApp.config(['$routeProvider','$stateProvider', '$urlRouterProvider', 
-	function($routeProvider,$stateProvider,$urlRouterProvider){
-	  $stateProvider
-	    .state('step1', {
-	      url: '/step_1',
-	      templateUrl: 'partials/create-vote.html'
-	    }) 
-	    .state('step2', {
-		      url: '/step_2',
-		      templateUrl: 'partials/config-vote.html',
-				controller: 'ConfigVoteCtrl'
+	function($routeProvider, $stateProvider, $urlRouterProvider){
+		$stateProvider
+		    .state('step1', {
+		    	url: '/step_1',
+		    	templateUrl: 'partials/create-vote.html'
 		    }) 
-		.state('step3', {
-			      url: '/step_3',
-			      templateUrl: 'partials/complete-vote.html',
-					controller: 'CompleteVoteCtrl'
-			    })
-	    .state('show', {
-	      url: '/finish/:param',
-	      templateUrl: 'partials/wizard/step_3.html'
-	    });
+		    .state('step2', {
+			    url: '/step_2',
+			    templateUrl: 'partials/config-vote.html',
+				controller: 'ConfigVoteCtrl'
+			}) 
+			.state('step3', {
+			    url: '/step_3',
+			    templateUrl: 'partials/complete-vote.html',
+				controller: 'CompleteVoteCtrl'
+			})
+		    .state('show', {
+		      	url: '/show/:param',
+		      	templateUrl: 'partials/show-vote.html'
+		    });
 	  
 	  	// in home page or pagination
-	  $routeProvider.when('/', {
-		  templateUrl: 'partials/home.html',
-		  controller: 'HomeCtrl'
-	      });
-
+		$routeProvider.when('/', {
+			templateUrl: 'partials/home.html',
+		  	controller: 'HomeCtrl'
+	    });
 
 }]);
