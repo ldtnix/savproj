f9589edc861ba412405ece9db2c222bfcbc1358d
unknown <tuanaphan@SGH504PT7D.kms.com.vn>
2015-03-24 10:36:13 +0700

    create html files and declare controllers

---
+++ src/main/webapp/js/controllers.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var savControllers = angular.module('savControllers', []);
+
+savControllers.controller('HomeCtrl', ['$scope', 
+	function($scope) {
+	
+	}]);
+
+savControllers.controller('CreateVoteCtrl', ['$scope', 
+	function($scope) {
+	
+	}]);
+
+savControllers.controller('ConfigVoteCtrl', ['$scope', 
+	function($scope) {
+	
+	}]);
+
+savControllers.controller('CompleteVoteCtrl', ['$scope', 
+	function($scope) {
+	
+	}]);
+
+savControllers.controller('ShowVoteCtrl', ['$scope', 
+	function($scope) {
+	
+	}]);
